5fdb4a6d239d1a8a794204de8eb85dd9ab0e9286
nRayen <rayen.ndiaye@gmail.com>
2024-09-17 00:02:09 +0200

    (main) : changed hourly preview layout

---
+++ src/components/HourlyPreviewDesktop.jsx
@@ -0,0 +1,225 @@
+import NuageuxSVG from "./svg/NuageuxSVG";
+
+const HourlyPreviewDesktop = () => {
+	return (
+			<div className="preview-h desktop">
+
+				<div className="day">
+					<p>Lun.</p>
+					<div className="hours">
+						<div className="h">
+							<h4>00h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>03h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>06h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>09h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>12h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>15h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>18h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>21h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="day">
+					<p>Mar.</p>
+					<div className="hours">
+						<div className="h">
+							<h4>00h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>03h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>06h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>09h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>12h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>15h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>18h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>21h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="day">
+					<p>Mer.</p>
+					<div className="hours">
+						<div className="h">
+							<h4>00h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>03h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>06h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>09h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>12h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>15h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>18h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>21h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="day">
+					<p>Jeu.</p>
+					<div className="hours">
+						<div className="h">
+							<h4>00h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>03h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>06h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>09h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>12h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>15h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>18h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+
+						<div className="h">
+							<h4>21h</h4>
+							<NuageuxSVG alt="nuage" />
+							<p>20°</p>
+						</div>
+					</div>
+				</div>
+
+
+
+			</div>
+	);
+};
+
+export default HourlyPreviewDesktop;
---
+++ src/components/HourlyPreviewMobile.jsx
@@ -0,0 +1,153 @@
+import NuageuxSVG from "./svg/NuageuxSVG";
+
+const HourlyPreviewMobile = () => {
+	return (
+			<div className="preview-h mobile">
+				<div className="h">
+					<h4>00h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>01h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>02h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>03h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>04h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>05h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>06h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>07h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>08h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>09h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>10h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>11h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>12h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>13h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>14h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>15h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>16h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>17h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>18h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>19h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>20h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>21h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>22h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+
+				<div className="h">
+					<h4>23h</h4>
+					<NuageuxSVG alt="nuage" />
+					<p>20°</p>
+				</div>
+			</div>
+	);
+};
+
+export default HourlyPreviewMobile;
