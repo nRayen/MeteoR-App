e0bf6298e2b84cdcf1a193e807cdae79fcfb5eb2
nRayen <rayen.ndiaye@gmail.com>
2024-09-30 17:14:57 +0200

    (main) : favorites redirect to city page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 import CrossSVG from "./svg/CrossSVG";
 import FavoriteSVG from "./svg/FavoriteSVG";
+import Link from "next/link";
 
 const Menu = ({ menuIsOpen,onHandleMenu }) => {
-	const [favorites, setFavorites] = useState(["Paris", "Bamako","Dubaï","Dakar", "Hanoi"]);
+	const [favorites, setFavorites] = useState(["Paris", "Bamako","Dubai","Dakar", "Hanoi"]);
 	console.log(favorites);
 
 	return (
@@ -14,7 +15,7 @@ const Menu = ({ menuIsOpen,onHandleMenu }) => {
 			    <h2>Favoris</h2>
 			    <ul>
 				    {favorites.map((fav) => (
-                        <li className="favorite"><p>{fav}</p><FavoriteSVG/></li>
+                        <li className="favorite"><Link href={"/"+fav}>{fav}</Link><FavoriteSVG/></li>
 				    ))}
 			    </ul>
             </div>
